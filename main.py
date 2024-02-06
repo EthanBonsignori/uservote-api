0ec99538cf30d4f061fdc4ba7b298069a8f037cc
Ethan Bonsignori <ebonsignori@gmail.com>
2024-02-05 20:22:19 -0800

    add CORSMiddleware, allow_origins

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+allow_origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,
+)
 
 @app.get("/")
 async def root():
