46c7663b7e0f9b7dd7005a353df9bdfb5ac7a6d1
Ethan Bonsignori <ebonsignori@gmail.com>
2024-02-06 19:52:32 -0800

    flesh out initial posts routes

--- database.py
+++ database.py
@@ -1,28 +1,34 @@
-from models import post
+from models.post import Post
+
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
-database = client.UserFeedback
-collection = database.posts
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
 
-async def find_one_post(id: int):
-    document = await collection.find_one({"id": id})
-    return document
+database = client.UserVote
+post_collection = database.post
+user_collection = database.user
 
 async def find_all_posts():
-    documents = collection.find({})
-    return [document async for document in documents]
+    documents = []
+    cursor = post_collection.find({})
+    async for document in cursor:
+        documents.append(Post(**document))
+    return documents
 
-async def create_post(new_post: post):
-    document = new_post.dict()
-    result = await collection.insert_one(document)
+async def find_one_post(id: int):
+    document = await post_collection.find_one({"id": id})
+    return document
+
+async def insert_post(post: Post):
+    document = post
+    result = await post_collection.insert_one(document)
     return result
 
-async def update_post(id: int, new_post: post):
-    document = new_post.dict()
-    result = await collection.update_one({"id": id}, {"$set": document})
+async def update_post(id: int, post: Post):
+    document = post
+    result = await post_collection.update_one({"id": id}, {"$set": document})
     return result
 
 async def delete_post(id: int):
-    result = await collection.delete_one({"id": id})
+    result = await post_collection.delete_one({"id": id})
     return result
